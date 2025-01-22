60910cc5018d769ce2678ecd3cd096a83b4ff508
steven-tan-telus <steven.tan@telus.com>
2025-01-22 12:47:36 -0500

    added routes,database,controller

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const mongodb = require("./data/database");
+const bodyParser = require("body-parser");
+
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Z-Key"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+  next();
+});
+app.use("/", require("./routes"));
+
+mongodb.initDb((err) => {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+});
---
+++ controllers/booksController.js
@@ -0,0 +1,87 @@
+const mongodb = require("../data/database");
+const { ObjectId } = require("mongodb");
+
+const getAllBooks = async (req, res) => {
+  const result = await mongodb.getDatabase().db().collection("books").find();
+  result.toArray().then((book) => {
+    res.setHeader("Content-Type", "application/json");
+    res.status(200).send(book);
+  });
+};
+
+const createBook = async (req, res) => {
+  const newBook = {
+    title: req.body.title,
+    author: req.body.author,
+    ISBN: req.body.ISBN,
+    genre: req.body.genre,
+    publishedYear: req.body.publishedYear,
+    copiesAvailable: req.body.copiesAvailable,
+    borrowedCount: req.body.borrowedCount,
+  };
+
+  const result = await mongodb
+    .getDatabase()
+    .db()
+    .collection("books")
+    .insertOne(newBook);
+
+  if (result.acknoweldged) {
+    res.status(204).send();
+  } else {
+    res
+      .status(500)
+      .json(result.error || "An error occurred while creating the contact");
+  }
+};
+
+const updateBook = async (req, res) => {
+  const bookId = ObjectId.createFromHexString(req.params.id);
+  const updateBook = {
+    title: req.body.title,
+    author: req.body.author,
+    ISBN: req.body.ISBN,
+    genre: req.body.genre,
+    publishedYear: req.body.publishedYear,
+    copiesAvailable: req.body.copiesAvailable,
+    borrowedCount: req.body.borrowedCount,
+  };
+
+  const result = await mongodb
+    .getDatabase()
+    .db()
+    .collection("books")
+    .updateOne({ _id: bookId }, { $set: updateBook });
+
+  if (result.modifiedCount == 1) {
+    res.status(200).send();
+  } else {
+    res
+      .status(500)
+      .json(result.error || "An error occurred while updating the contact");
+  }
+};
+
+const deleteBook = async (req, res) => {
+  const bookId = ObjectId.createFromHexString(req.params.id);
+  const result = await mongodb
+    .getDatabase()
+    .db()
+    .collection("books")
+    .deleteOne({ _id: bookId });
+
+  if (result.deletedCount == 1) {
+    res.status(200).send();
+  } else {
+    res
+      .status(500)
+      .json(result.error || "An error occurred while deleting the contact");
+  }
+};
+
+module.exports = {
+  getAllBooks,
+  createBook,
+  updateBook,
+  deleteBook,
+};

073531e173d3852974ae4f35232fe98cde8113e6
steven-tan-telus <steven.tan@telus.com>
2025-01-22 11:38:13 -0500

    initial setup

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+
+
+app.get('/',(req,res,next) => {
+	res.send('Hello World!');
+});
+
+app.listen(PORT,()=>{
+	console.log(`server is listening to port ${PORT}`);
+});
